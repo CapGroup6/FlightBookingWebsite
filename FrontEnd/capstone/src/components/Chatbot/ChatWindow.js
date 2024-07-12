b7bd89da68f2c19e6f50f7214c8b0ba329d6cecc
chelsiexie7 <chelsiexie7@gmail.com>
2024-07-12 10:51:57 -0700

    add sort function

--- FrontEnd/capstone/src/components/Chatbot/ChatWindow.js
+++ FrontEnd/capstone/src/components/Chatbot/ChatWindow.js
@@ -3,12 +3,19 @@ import React from 'react';
 const ChatWindow = ({ conversations }) => {
   if (!conversations) return null;
 
+  const renderMessageText = (text) => {
+    if (typeof text === 'object' && text !== null && text.data) {
+      return text.data;
+    }
+    return text;
+  };
+
   return (
     <div className="flex-grow p-4 bg-blue-50">
       {conversations.map((message) => (
         <div key={message.id} className={`flex ${message.user === 'user' ? 'justify-end' : 'justify-start'} mb-4`}>
           <div className={`p-4 rounded-lg ${message.user === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}>
-            {message.text}
+            {renderMessageText(message.text)}
           </div>
         </div>
       ))}
---
+++ FrontEnd/capstone/src/components/Result/Sort.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Select from 'react-select';
+import styles from '../../styles/SearchForm.module.css';
+
+const sortOptions = [
+  { label: 'Price', value: 'price' },
+  { label: 'Duration', value: 'duration' },
+  { label: 'Recommend', value: 'recommend' }
+];
+
+const Sort = ({ sortCriteria, setSortCriteria }) => {
+  return (
+    <div className={styles.noBorder}>
+      <Select
+        classNamePrefix="noBorder"
+        placeholder="Sort By"
+        value={sortCriteria}
+        onChange={setSortCriteria}
+        options={sortOptions}
+        styles={{
+          control: (base) => ({
+            ...base,
+            border: 'none',
+            boxShadow: 'none',
+            minHeight: 'auto',
+            '&:hover': {
+              border: 'none'
+            },
+            width: '150px', // 调整宽度以适应内容
+          }),
+          valueContainer: (base) => ({
+            ...base,
+            padding: '0'
+          }),
+          input: (base) => ({
+            ...base,
+            margin: '0'
+          }),
+          indicatorSeparator: (base) => ({
+            display: 'none'
+          }),
+          indicatorsContainer: (base) => ({
+            ...base,
+            padding: '0'
+          })
+        }}
+      />
+    </div>
+  );
+};
+
+export default Sort;
