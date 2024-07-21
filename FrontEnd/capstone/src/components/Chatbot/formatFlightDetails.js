59a7d7e7de9050317892a4a19594efbbd771f78f
chelsiexie7 <chelsiexie7@gmail.com>
2024-07-21 15:35:05 -0700

    fix the bug of cannot show date in chatbot

--- FrontEnd/capstone/src/components/Chatbot/formatFlightDetails.js
+++ FrontEnd/capstone/src/components/Chatbot/formatFlightDetails.js
@@ -33,7 +33,12 @@ const calculateTotalDuration = (segments) => {
 
 const formatTime = (timeStr) => {
     const date = new Date(timeStr);
-    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return date.toLocaleTimeString([], { 
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit', 
+        hour: '2-digit', 
+        minute: '2-digit' });
 };
 
 const convertPriceToCAD = async (price) => {
